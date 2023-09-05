1bd2f7360a80d9782eb7a15593e6394a15dff4f7
humantiy <120549792+humantiy@users.noreply.github.com>
2023-09-05 23:06:55 +0800

    Add files via upload

---
+++ nav/js/index.js
@@ -0,0 +1,11 @@
+/*
+
+Messed around with the Randomize Everything Sublime Text 3 Plugin.
+https://packagecontrol.io/packages/Random%20Everything
+
+Probably there's a way to do all that with a fancy mixin instead
+of hard-coding the values, but hey.
+
+Feel free to post it in the comments.
+
+*/
